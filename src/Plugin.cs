1532594c63b15b9254bc23e53e2f041c4c21c1b9
Timo <T.Euteneuer.TE@gmail.com>
2026-07-26 14:48:37 +0200

    Add `ZeepSDK` imports to `Plugin` for extended SDK functionality. Im cool af.

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -5,6 +5,7 @@ using Showdown4.Config;
 using Showdown4.States;
 using Showdown4.States.Master;
 using Showdown4.Utils;
+using ZeepSDK;
 using ZeepSDK.ChatCommands;
 using ZeepSDK.Storage;
 using ZeepSDK.UI;
